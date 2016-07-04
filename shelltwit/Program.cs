f283e59abd7c74d3f8c6d712ce6518d2a4c77008
sebagomez <sebagomezcorrea@outlook.com>
2016-07-04 14:07:37 -0300

    fix broken build

--- shelltwit/Program.cs
+++ shelltwit/Program.cs
@@ -19,8 +19,8 @@ namespace shelltwit
 		const string HELP = "/?";
 		const string MENTIONS = "/m";
 
-		//public const string CONSUMER_KEY = "<CONSUMER_KEY HERE>";
-		//public const string CONSUMER_SECRET = "<CONSUMER_SECRET HERE>";
+		public const string CONSUMER_KEY = "<CONSUMER_KEY HERE>";
+		public const string CONSUMER_SECRET = "<CONSUMER_SECRET HERE>";
 
 		static void Main(string[] args)
 		{
