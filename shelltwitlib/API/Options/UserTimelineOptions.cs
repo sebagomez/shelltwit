b758e758b2190c599c7bdfe2bfe1307b33140383
sebagomez <sebagomezcorrea@outlook.com>
2016-07-25 13:06:14 -0300

    UserTimeline implemented

---
+++ shelltwitlib/API/Options/UserTimelineOptions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Sebagomez.ShelltwitLib.API.Options
+{
+	public class UserTimelineOptions : TwitterOptions
+	{
+		public long UserId { get; set; }
+		public string ScreenName { get; set; }
+		public int Count { get; set; }
+		public long SinceId { get; set; }
+		public long MaxId { get; set; }
+		public bool ExcludeReplies { get; set; }
+		public bool IncludeRTs { get; set; } = true;
+
+		public override Dictionary<string, string> GetParameters()
+		{
+			Dictionary<string, string> parameters = new Dictionary<string, string>();
+			if (UserId != 0)
+				parameters.Add("user_id", UserId.ToString());
+			if (!string.IsNullOrEmpty(ScreenName))
+				parameters.Add("screen_name", ScreenName);
+			if (SinceId != 0)
+				parameters.Add("since_id", SinceId.ToString());
+			if (Count != 0)
+				parameters.Add("count", Count.ToString());
+			if (MaxId != 0)
+				parameters.Add("max_id", MaxId.ToString());
+			if (ExcludeReplies)
+				parameters.Add("exclude_replies", ExcludeReplies.ToString());
+			if (!IncludeRTs)
+				parameters.Add("include_rts", IncludeRTs.ToString());
+
+			return parameters;
+		}
+	}
+}
--- shelltwitlib/API/Tweets/UserTimeline.cs
+++ shelltwitlib/API/Tweets/UserTimeline.cs
@@ -8,21 +8,16 @@ using Sebagomez.ShelltwitLib.Web;
 
 namespace Sebagomez.ShelltwitLib.API.Tweets
 {
-	public class Timeline
+	public class UserTimeline
 	{
-		const string HOME_TIMELINE = "https://api.twitter.com/1.1/statuses/home_timeline.json";
+		const string USER_TIMELINE = "https://api.twitter.com/1.1/statuses/user_timeline.json";
 
-		public static List<Status> GetTimeline()
-		{
-			return GetTimeline(new TimelineOptions());
-		}
-
-		public static List<Status> GetTimeline(TimelineOptions options)
+		public static List<Status> GetUserTimeline(UserTimelineOptions options)
 		{
 			if (options.User == null)
 				options.User = AuthenticatedUser.LoadCredentials();
 
-			HttpWebRequest req = OAuthHelper.GetRequest(HttpMethod.GET, HOME_TIMELINE, options);
+			HttpWebRequest req = OAuthHelper.GetRequest(HttpMethod.GET, USER_TIMELINE, options);
 			HttpWebResponse response = (HttpWebResponse)req.GetResponse();
 
 			if (response.StatusCode != HttpStatusCode.OK)
