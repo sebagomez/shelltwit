3d078fe368f8649df4410e071099c637eb2d54c8
sebagomez <sebagomezcorrea@outlook.com>
2017-02-03 11:38:50 -0300

    finnaly building in linux

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 cp shelltwit/Properties/AssemblyInfo.cs shelltwitcore/Properties/ --verbose
 cp ./CommonAssemblyInfo.cs ./shelltwitcore/Properties/ --verbose
 cp ./shelltwit/*.cs ./shelltwitcore/ --verbose
-cp ./shelltwit/*.data ./shelltwitcore/ --verbose
-cp ./shelltwit/*.data ./shelltwitcore/bin/Debug/netcoreapp1.1/ --verbose
-cp -r ./shelltwitlib/*/*.cs ./shelltwitcore/ --verbose
+rsync -avh --include="*.cs" --exclude="*.*" ./shelltwitlib/ ./shelltwitcore
 cp ./BitLyHelper/Util.cs ./BitLyHelper/BitLyUtil.cs --verbose
 cp ./BitLyHelper/BitLyUtil.cs ./shelltwitcore/ --verbose
 rm ./BitLyHelper/BitLyUtil.cs --verbose
