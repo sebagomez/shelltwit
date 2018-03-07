0ff779e37f24366431769e240d004cc9996bfd7c
seba gomez <sebagomezcorrea@outlook.com>
2018-03-07 16:20:51 -0300

    7.3.1 tag

--- Docker/build.bat
+++ Docker/build.bat
@@ -1,2 +1,2 @@
-docker build --build-arg TAG=7.2 -t sebagomez/shelltwit:7.2 .
-docker push sebagomez/shelltwit:7.2
+docker build --build-arg TAG=7.3.1 -t sebagomez/shelltwit:7.3.1 .
+docker push sebagomez/shelltwit:7.3.1
