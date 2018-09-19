d28b70072b2363041e07af42689b8b959f7d80c1
seba gomez <sebagomezcorrea@outlook.com>
2018-09-19 13:34:33 -0300

    improved retweted and quoted twits visualization (I hope)

--- src/shelltwitlib/Entities/Status.cs
+++ src/shelltwitlib/Entities/Status.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Sebagomez.ShelltwitLib.Entities
 {
@@ -8,29 +9,17 @@ namespace Sebagomez.ShelltwitLib.Entities
 	{
 		public string created_at { get; set; }
 		public long id { get; set; }
-		public string id_str { get; set; }
-		public string text { get; set; }
 		public string full_text { get; set; }
-		public bool truncated { get; set; }
 		public string source { get; set; }
-		public long? in_reply_to_status_id { get; set; }
-		public string in_reply_to_status_id_str { get; set; }
-		public long? in_reply_to_user_id { get; set; }
-		public string in_reply_to_user_id_str { get; set; }
-		public string in_reply_to_screen_name { get; set; }
+		public object in_reply_to_status_id { get; set; }
+		public object in_reply_to_user_id { get; set; }
+		public object in_reply_to_screen_name { get; set; }
 		public User user { get; set; }
-		public object geo { get; set; }
-		public object coordinates { get; set; }
-		public object contributors { get; set; }
-		public Status retweeted_status { get; set; }
-		public Status extended_tweet { get; set; }
 		public bool is_quote_status { get; set; }
-		public int retweet_count { get; set; }
-		public int favorite_count { get; set; }
-		public bool favorited { get; set; }
-		public bool retweeted { get; set; }
-		public bool? possibly_sensitive { get; set; }
 		public string lang { get; set; }
+		public Status retweeted_status { get; set; }
+		public long quoted_status_id { get; set; }
+		public Status quoted_status { get; set; }
 
 		string m_resolvedText;
 		public string ResolvedText
@@ -38,7 +27,14 @@ namespace Sebagomez.ShelltwitLib.Entities
 			get
 			{
 				if (string.IsNullOrEmpty(m_resolvedText))
-					m_resolvedText = extended_tweet != null ? extended_tweet.full_text : (retweeted_status != null ? retweeted_status.ResolvedText : (!string.IsNullOrEmpty(full_text) ? full_text : text)); // sometimes text is truncated while the retweeted text isn't
+				{
+					if (quoted_status != null)
+						m_resolvedText = $"{full_text}{Environment.NewLine}\"{quoted_status.full_text}\"";
+					else if (retweeted_status != null)
+						m_resolvedText = retweeted_status.full_text;
+					else
+						m_resolvedText = full_text;
+				}
 
 				return m_resolvedText;
 			}
