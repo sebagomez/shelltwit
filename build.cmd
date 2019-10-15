07cf0155fc0bbfa35f26f70558937cf6b3fca31f
Sebastián Gómez <sebagomezcorrea@outlook.com>
2019-10-14 22:03:09 -0300

    target the right net core runtime in docker image

--- build.cmd
+++ build.cmd
@@ -3,4 +3,4 @@ set Config=%1
 if %1. == . set Config=Release
 
 dotnet build --force -c %Config% shelltwit.sln
-dotnet publish -c %Config% -o ..\bin shelltwit.sln 
+dotnet publish -c %Config% -o .\bin shelltwit.sln 
